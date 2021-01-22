81da6d45700400f1aa87f8c665a518ea99062704
mil <borg@mi.t.u-tokyo.ac.jp>
2021-01-22 15:15:09 +0900

    added notebooks

---
+++ notebooks/run_backup.sh
@@ -0,0 +1,5 @@
+python robust_rewards.py \
+    --seed 6 \
+    --name TEST_BNN \
+    --env_id Safexp-PointGoalThree0-v0 \
+    --precollected_trajectories ./data/local/experiment/diversity_is_all_you_need_seed=10_name=fixed_eval_determinism_12042020_003752_number_skills=50_number_epochs=500_alpha=0.5_env_id=Safexp-PointGoalThree0-v0/comparison_collector.pkl
