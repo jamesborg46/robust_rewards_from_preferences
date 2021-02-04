0938745aece8ea2d760c94cfad349ec0cc638329
root <root@g0001.abci.local>
2021-02-04 13:03:30 +0000

    added reversed env

--- envs/custom_safety_envs.py
+++ envs/custom_safety_envs.py
@@ -20,14 +20,14 @@ common = {
     'hazards_keepout': 0.18,
     'constrain_hazards': True,
     'observe_hazards': True,
-    'robot_locations': [(0, -1.5)],
-    'robot_rot': np.pi * (1/2),
     'hazards_size': 0.4,
     'placements_extents': [-1.5, -1.5, 1.5, 1.5],
 }
 
 
 front = {
+    'robot_locations': [(0, -1.5)],
+    'robot_rot': np.pi * (1/2),
     'goal_locations': [(0, 1.5)],
     'hazards_num': 3,
     'vases_num': 0,
@@ -37,6 +37,8 @@ front = {
 }
 
 behind = {
+    'robot_locations': [(0, -1.5)],
+    'robot_rot': np.pi * (1/2),
     'goal_locations': [(0, 0)],
     'hazards_num': 3,
     'vases_num': 0,
@@ -46,6 +48,8 @@ behind = {
 }
 
 three = {
+    'robot_locations': [(0, -1.5)],
+    'robot_rot': np.pi * (1/2),
     'goal_locations': [(0, 1.5)],
     'hazards_num': 1,
     'vases_num': 1,
@@ -66,9 +70,35 @@ three = {
     'pillars_cost': 30,
 }
 
+three_reversed = {
+    'robot_locations': [(0, 1.5)],
+    'robot_rot': np.pi * -(1/2),
+    'goal_locations': [(0, -1.5)],
+    'hazards_num': 1,
+    'vases_num': 1,
+    'pillars_num': 1,
+    'observe_vases': True,
+    'observe_pillars': True,
+    'constrain_vases': True,
+    'constrain_pillars': True,
+    'hazards_placements': None,
+    'hazards_locations': [(0, 0)],
+    'vases_placements': None,
+    'vases_locations': [(-1.3, -0.9)],
+    'pillars_placements': None,
+    'pillars_locations': [(1.3, -0.9)],
+    'pillars_size': 0.2,
+    'vases_size': 0.2,
+    'vases_contact_cost': 30,
+    'pillars_cost': 30,
+}
+
 irl_goal_base = suite.bench_base.copy('IRLGoal', common)
 irl_goal_base.register('Front', front)
 irl_goal_base.register('Behind', behind)
 
 # Savexp-PointIRLGoalThree-v0'
 irl_goal_base.register('Three', three)
+
+# Savexp-PointIRLGoalThreeReversed-v0'
+irl_goal_base.register('ThreeReversed', three_reversed)
