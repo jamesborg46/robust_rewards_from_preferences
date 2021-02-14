559228e362b4b46ab02330dc96380ebe9794e79e
Indoril007 <jamesborg46@gmail.com>
2021-02-14 08:49:02 +0000

    Fixed IRL experiments

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 python robust_rewards.py \
     --seed 6 \
-    --name TEST_$1 \
+    --name FIXED_IRL_$1 \
     --env_id 'Safexp-PointIRLGoalThree-v0' \
     --number_epochs 1000 \
     --snapshot_gap 200 \
-    --steps_per_epoch 2000 \
+    --steps_per_epoch 10000 \
     --max_episode_length 1000 \
-    --n_workers 2 \
+    --n_workers 10 \
     --policy "GaussianMLPPolicy(env.spec,
                                 hidden_sizes=[32, 32],
                                 hidden_nonlinearity=torch.tanh,
@@ -21,7 +21,7 @@ python robust_rewards.py \
                                      minibatch_size=256)" \
     --label_scheduler "LabelAnnealer(number_epochs=kwargs['number_epochs'],
                                      final_labels=1000,
-                                     pretrain_labels=200)" \
+                                     pretrain_labels=100)" \
     --data_collector "SyntheticPreferenceCollector(env.spec,
                                                    label_scheduler,
                                                    segment_length=1,
@@ -29,7 +29,7 @@ python robust_rewards.py \
     --reward_predictor "PrefMLP(env.spec,
                                 hidden_sizes=[64, 64],
                                 preference_collector=data_collector,
-                                pretrain_epochs=10,
+                                pretrain_epochs=50,
                                 iterations_per_epoch=100,
                                 minibatch_size=256)" \
     --algo "IrlTRPO(env.spec,
@@ -40,7 +40,9 @@ python robust_rewards.py \
                     policy=policy,
                     value_function=value_function,
                     vf_optimizer=vf_optimizer,
-                    render_freq=2,
+                    render_freq=200,
+                    entropy_method='regularized',
+                    policy_ent_coeff=0.1,
                     discount=1,
                     gae_lambda=1,)" \
     --ray
